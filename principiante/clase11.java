41192a97c8ea4190a85c39d7aa5f06621cb3f6dd
OneSantiro360 <johndoe@example.com>
2022-07-28 00:11:55 -0500

    commit

--- principiante/clase11.java
+++ principiante/clase11.java
@@ -1,9 +1,18 @@
 //Estructuras de control (Switch - Case)
+import java.util.Scanner;
 
 public class clase11 {
   public static void main(String args[]){
-    int num1=5, num2=3, resultado=0;
-    int parametro=3;
+    Scanner in=new Scanner(System.in);
+    int num1=0, num2=0, resultado=0;
+    int parametro=0;
+
+    System.out.println("Cual es su primer numero");
+    num1=in.nextInt();
+    System.out.println("Cual es su segundo numero");
+    num2=in.nextInt();
+    System.out.println("Cual es la operacion que quiere realizar; Multiplicar: 1, resta: 2, suma: 3");
+    parametro=in.nextInt();
 
     switch(parametro){
       case 1: resultado=num1*num2;
@@ -16,7 +25,7 @@ public class clase11 {
       System.out.println("El resultado de la suma es de "+resultado);
       break;
 
-      default: System.out.println("ERROr, la opcion no existe");
+      default: System.out.println("ERROR, la opcion no existe");
     }
   }
 }
