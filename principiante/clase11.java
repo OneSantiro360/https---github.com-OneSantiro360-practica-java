4363eef012acd4277683a1677a41add120ae47e7
OneSantiro360 <johndoe@example.com>
2022-07-27 23:59:49 -0500

    commit

--- principiante/clase11.java
+++ principiante/clase11.java
@@ -1,5 +1,22 @@
+//Estructuras de control (Switch - Case)
+
 public class clase11 {
   public static void main(String args[]){
-    
+    int num1=5, num2=3, resultado=0;
+    int parametro=3;
+
+    switch(parametro){
+      case 1: resultado=num1*num2;
+      System.out.println("El resultado de la multiplicacion es de "+resultado);
+      break;
+      case 2: resultado=num1-num2;
+      System.out.println("El resultado de la resta es de "+resultado);
+      break;
+      case 3: resultado=num1+num2;
+      System.out.println("El resultado de la suma es de "+resultado);
+      break;
+
+      default: System.out.println("ERROr, la opcion no existe");
+    }
   }
 }
