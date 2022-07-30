fb52c053cc57062f1c2ca4ee9982afb5606f108f
OneSantiro360 <johndoe@example.com>
2022-07-30 14:56:58 -0500

    commit

---
+++ principiante/clase16.java
@@ -0,0 +1,12 @@
+//Sucesión fibonacci
+public class clase16{
+  public static void main(String args[]){
+    int a=0, b=1, c=0;
+    for(int i=0; i<10; i++){
+      System.out.print(a+",");
+      c=a+b;
+      a=b;
+      b=c;
+    }
+  }
+}
